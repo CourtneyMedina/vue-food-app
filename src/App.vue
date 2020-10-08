fff27d67d0a62fbf47d53be8be17e3ac853f3358
CourtneyMedina <courtney.medina.222@gmail.com>
2020-10-07 22:29:06 -0500

    added nav bar and card components

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,34 @@
 <template>
-  
+  <div class="container">
+    <NavBar></NavBar>
+    <Card v-for="food in foods" 
+            :key="food.product" 
+            :price="food.price" 
+            :productname="food.product"
+            :manufacturer="food.manufacturer" 
+            :rating="food.rating"></Card>
+
+  </div>
 </template>
 
 <script>
-
+import NavBar from "./components/NavBar.vue";
+import Card from "./components/Card.vue";
+import * as foods from "./assets/foods.json";
 
 export default {
- 
+  components: {
+    NavBar,
+    Card,
+  },
+    setup: {
+    foods,
+    }
 }
+
+// let cart = [];
+
+
 </script>
 
 <style>
@@ -18,5 +39,14 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.container {
+  margin: 3rem;
+  display: grid;
+  grid-template-columns: auto;
+  column-gap: 1rem;
+  row-gap: 0.1rem;
+  background-color: red;
 }
 </style>
