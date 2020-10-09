932040129ea1639ae0fac3d5b9e1fc11064867be
CourtneyMedina <courtney.medina.222@gmail.com>
2020-10-08 21:58:04 -0500

    update

---
+++ src/components/Card.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="card">
+    <img src="https://www.flaticon.com/svg/static/icons/svg/1377/1377194.svg" class="card-img-top" alt="Image Failed to Load">
+        <div class="card-body">
+            <h5 class="card-title" :product="product" id="product-name">{{product}}</h5>
+            <p class="card-text" :manufacturer="manufacturer" id="brand">{{manufacturer}}</p>
+            <p class="card-text" :rating="rating" id="rating">Rating: {{rating}}</p>
+            <p class="card-text" :price="price" id="price">{{price}}</p>
+            <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    props: {
+        key: String,
+        product: String,
+        manufacturer: String,
+        rating: Number,
+        price: Number,
+    }
+
+}
+</script>
+
+<style>
+.card {
+    width: 18rem;
+    padding: 0.5rem;
+    margin: 1rem 0rem;
+}
+
+
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,53 @@
+<template>
+    <nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-primary">
+    <a class="navbar-brand" href="#">
+        <img src="https://www.flaticon.com/svg/static/icons/svg/1377/1377194.svg" 
+      width="30" height="30" class="d-inline-block align-top" alt="" loading="lazy">
+
+      Courtney's Market
+    </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarText">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Menu
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+    </ul>
+    <span class="navbar-text">
+      Wallet: $60
+    </span>
+  </div>
+
+ 
+</nav>
+
+
+
+
+</template>
+
+<script>
+
+export default {
+  
+}
+
+</script>
+
+<style>
+
+
+</style>
